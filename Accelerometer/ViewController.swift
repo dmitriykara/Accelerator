5e341e6ae7a773b7cab425a8d1ca41f47a4a8035
Dmitriy Kara <kara@group-ib.com>
2021-03-19 18:23:17 +0300

    Engine 2.0

--- Accelerometer/ViewController.swift
+++ Accelerometer/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum Motion {
+    case Left
+    case Right
+    case Forward
+    case Backward
+    case Stable
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var resultsLabel: UILabel!
+    @IBOutlet var XLabel: UILabel!
+    @IBOutlet var YLabel: UILabel!
+    @IBOutlet var ZLabel: UILabel!
+    
+    let changeX = 0.25;
+    let changeY = 0.30;
+    let iterval = 0.07; // 70 ms
+    let motionManager = CMMotionManager()
+    var timer: Timer!
+    var x = 0.0;
+    var y = 0.0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        motionManager.startAccelerometerUpdates()
+        
+        timer = Timer.scheduledTimer(timeInterval: iterval, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+    }
+    
+    func getMotion() -> Motion {
+        if let accelerometerData = motionManager.accelerometerData {
+            let new_x = accelerometerData.acceleration.x;
+            let new_y = accelerometerData.acceleration.y;
+            
+            if x == 0.0 && y == 0.0 {
+                x = new_x
+                y = new_y
+                return Motion.Stable
+            }
+            
+            XLabel.text = "\(accelerometerData.acceleration.x)"
+            YLabel.text = "\(accelerometerData.acceleration.y)"
+            ZLabel.text = "\(accelerometerData.acceleration.z)"
+            
+            let xUp = (new_x > x) && (new_x - x) > changeX;
+            let xDown = (new_x < x) && (x - new_x) > changeX;
+            let yUp = (new_y > y) && (new_y - y) > changeY;
+            let yDown = (new_y < y) && (x - new_y) > changeY;
+            
+            if xUp {
+                return Motion.Right
+            }
+            if xDown {
+                return Motion.Left
+            }
+            if yUp {
+                return Motion.Forward
+            }
+            if yDown {
+                return Motion.Backward
+            }
+        }
+        return Motion.Stable
+    }
+    
+    @objc func update() {
+        let  motion = getMotion()
+        print(motion)
+        resultsLabel.text = "\(motion)"
+        if motion == Motion.Stable {
+            resultsLabel.textColor = UIColor.green
+        } else {
+            resultsLabel.textColor = UIColor.red
+        }
+    }
+}
