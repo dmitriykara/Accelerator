80d1d3ef073fe87dded2b044ecbfe29194e46f13
Dmitriy Kara <kara@group-ib.com>
2021-03-26 01:43:39 +0300

    Add send request

--- Accelerometer/ViewController.swift
+++ Accelerometer/ViewController.swift
@@ -79,10 +79,64 @@ class ViewController: UIViewController {
         let  motion = getMotion()
         print(motion)
         resultsLabel.text = "\(motion)"
+        submitAction(motion: motion)
         if motion == Motion.Stable {
             resultsLabel.textColor = UIColor.green
         } else {
             resultsLabel.textColor = UIColor.red
         }
+    }
+    
+    func submitAction(motion: Motion) {
+        
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = [
+            "motion": motion,
+        ]
+        
+        //create the url with URL
+        let url = URL(string: "http://localhost:5000/")! //change the url
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                } else {
+                    print(data)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
 }
