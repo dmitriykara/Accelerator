e05f3f1506360790b390bf7d54c81d3a22a8fdbd
Dmitriy Kara <kara@group-ib.com>
2021-04-08 19:20:43 +0300

    Fix front+back

--- server/client.py
+++ server/client.py
@@ -2,12 +2,55 @@ import requests
 import random
 import time
 
+letters = {
+    "Left": "йцуфывячс",
+    "Right": "нгшролть",
+    "Forward": "щзхъджэбю",
+    "Backward": "кеапми",
+}
+
 motions = ["Forward", "Backward", "Left", "Right", "Stable", "Up", "Down", "MoveLeft", "MoveRight"]
 
+input = [
+    "Backward", # п
+    "Right",    # р
+    "Backward", # и
+    "Backward", # м
+    "Backward", # е
+    "Right",    # р
+    "Up",       # _
+    "Right",    # т
+    "Backward", # е
+    "Backward", # к
+    "Left",     # с
+    "Right",    # т
+    "Backward", # а
+    "Up",       # _
+]
+
+input2 = [
+    "Left",     # п
+    "Right",    # р
+    "Left",     # и
+    "Left",     # м
+    "Left",     # е
+    "Right",    # р
+    "Up",       # _
+    "Right",    # т
+    "Left",     # е
+    "Left",     # к
+    "MoveLeft", # с
+    "Right",    # т
+    "Left",     # а
+    "Up",       # _
+]
+
+i = 0
 while True: 
     msg = {
-        "motion": motions[random.randint(0, len(motions) - 1)],
+        "motion": input[i],
     }
+    i+= 1
     resp = requests.post('http://0.0.0.0:6000/motion', json=msg)
     print(resp.status_code)
     time.sleep(1)
